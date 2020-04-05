590ad380eba1bddc53d851566b63a933e4de3fed
詹晟 <sillyknight@zhanshengdeMacBook-Pro.local>
2020-04-05 00:12:53 -0400

    backend db upload

---
+++ js_backend/config.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config();
+module.exports = {
+    atlas: process.env.DBURL,
+    appdb: process.env.DB,
+    userscoll: process.env.USERSCOLL,
+    port: process.env.PORT
+};
